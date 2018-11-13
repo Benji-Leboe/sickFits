5ce570113fad345867fd522d8a28fabaee7d2b56
Benji Leboe <benjamin.r.leboe@gmail.com>
2018-11-12 23:23:53 -0800

    style page component and config theme

--- sick-fits/frontend/components/Page.jsx
+++ sick-fits/frontend/components/Page.jsx
@@ -1,15 +1,25 @@
 import React, { Component } from 'react';
 import Header from './Header.jsx';
 import Meta from './Meta.jsx';
-import styled from 'styled-components';
+import styled, { ThemeProvider, injectGlobal } from 'styled-components';
+
+const theme = {
+  red: '#FF0000',
+  black: '#393939',
+  grey: '#3A3A3A',
+  lightgrey: '#E1E1E1',
+  offWhite: '#EDEDED',
+  maxWidth: '1000px',
+  bs: '0 12px 24px 0 rgba(0, 0, 0, 0.09)'
+}
 
 const StyledPage = styled.div`
   background: white;
-  color: black;
+  color: ${ props => props.theme.black };
 `;
 
 const InnerPage = styled.div`
-  max-width: 1000px;
+  max-width: ${ props => props.theme.maxWidth };
   margin: 0 auto;
   padding: 2rem;
 `;
@@ -17,15 +27,17 @@ const InnerPage = styled.div`
 class Page extends Component {
   render() {
     return (
-      <StyledPage>
-        <Meta />
-        <Header />
+      <ThemeProvider theme={ theme }>
+        <StyledPage>
+          <Meta />
+          <Header />
 
-        <InnerPage>
-        {this.props.children}
-        </InnerPage>
-      
-      </StyledPage>
+          <InnerPage>
+          {this.props.children}
+          </InnerPage>
+        
+        </StyledPage>
+      </ThemeProvider>
     );
   }
 }
