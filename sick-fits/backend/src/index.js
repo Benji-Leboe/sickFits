44350daf7998b81172169410435c5f826be407d4
Benji Leboe <benjamin.r.leboe@gmail.com>
2018-11-13 21:25:38 -0800

    init config for GQL Yoga server

--- sick-fits/backend/src/index.js
+++ sick-fits/backend/src/index.js
@@ -1,1 +1,17 @@
-// let's go!
+require('dotenv').config({ path: './variables.env'});
+const createServer = require('./createServer');
+const db = require('./db');
+
+const server = createServer();
+
+server.start(
+  {
+    cors: {
+      credentials: true,
+      origin: process.env.FRONTEND_URL
+    }
+  }, 
+  res => {
+    console.log(`Server running on ${res.port}`);
+  }
+);
